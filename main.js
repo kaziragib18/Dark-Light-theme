402db51566181e62256b213b63662e1ccbc099a4
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-07-08 23:19:13 +0600

    js fix

--- main.js
+++ main.js
@@ -8,17 +8,19 @@ const textBox = document.getElementById('text-box');
 
 // Dark or Light Images
 function imageMode(color) {
-    image1.src = `img/undraw_proud_coder_${color}.svg`;
-    image2.src = `img/undraw_feeling_proud_${color}.svg`;
-    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+    image1.src = `img/coder_${color}.svg`;
+    image2.src = `img/proud_${color}.svg`;
+    image3.src = `img/idea_${color}.svg`;
 }
+
 function toggleDarkLightMode(isDark) {
     nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
     isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    isDark ? imageMode('dark') : imageMode('light');  
-}  
+    isDark ? imageMode('dark') : imageMode('light');
+}
+
 // Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
@@ -31,6 +33,7 @@ function switchTheme(event) {
         toggleDarkLightMode(false);
     }
 }
+
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
 
