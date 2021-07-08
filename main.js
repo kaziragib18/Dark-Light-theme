c86df3b58022f459ab232e1176e60748e6ab3bce
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-07-08 22:25:39 +0600

    main js  added

---
+++ main.js
@@ -0,0 +1,6 @@
+// Dark or Light Images
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
